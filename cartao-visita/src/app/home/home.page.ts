20ad4215c538c4850c698b6cac170a2090c93bf5
java2124 <rafabf1508@gmail.com>
2022-07-17 20:34:00 -0300

    dark and light mode

--- cartao-visita/src/app/home/home.page.ts
+++ cartao-visita/src/app/home/home.page.ts
@@ -9,7 +9,13 @@ export class HomePage {
 
   public fullstack = true;
   public salesforce = false;
-  public itau = false;
+  public dark = true;
+  public light = false;
+  public toolbarIntroduction = 'toolbar-introduction-light';
+  public textThemeColor = 'text-black';
+  public backgroundTheme = 'background-light';
+  public footerColor = 'footer-light';
+  public avatarStyle = 'avatarDiv animation-body gradient-light';
 
   constructor() {}
 
@@ -36,7 +42,6 @@ export class HomePage {
   showFullstack(){
     this.salesforce = false;
     this.fullstack = true;
-    this.itau = false;
 
     setTimeout(() =>
     {
@@ -48,22 +53,30 @@ export class HomePage {
   showSalesforce(){
     this.salesforce = true;
     this.fullstack = false;
-    this.itau = false;
-
-    setTimeout(() => {
-      this.showItau();
-    },
-    1500);
-  }
-
-  showItau(){
-    this.salesforce = false;
-    this.fullstack = false;
-    this.itau = true;
 
     setTimeout(() => {
       this.showFullstack();
     },
     1500);
+  }
+
+  public showDark(){
+    this.dark = false;
+    this.light = true;
+    this.toolbarIntroduction = 'toolbar-introduction-dark';
+    this.textThemeColor = 'text-white';
+    this.backgroundTheme = 'background-dark';
+    this.footerColor = 'footer-dark';
+    this.avatarStyle = 'avatarDiv animation-body gradient-dark';
+  }
+
+  public showLight(){
+    this.light = false;
+    this.dark = true;
+    this.toolbarIntroduction = 'toolbar-introduction-light';
+    this.textThemeColor = 'text-black';
+    this.backgroundTheme = 'background-light';
+    this.footerColor = 'footer-light';
+    this.avatarStyle = 'avatarDiv animation-body gradient-light';
   }
 }
